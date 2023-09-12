4ee5af097fa8de4699d212a2c775fbcedc915ac2
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-09-12 14:53:56 -0300

    Create tests to male cases

--- src/test/java/bmi/BodyMassIndexTest.java
+++ src/test/java/bmi/BodyMassIndexTest.java
@@ -40,4 +40,39 @@ class BodyMassIndexTest {
         bodyMassIndex.setWeight(32.3);
         assertEquals("Obese", bodyMassIndex.getBodyMassIndex());
     }
+
+    @Test
+    public void shouldReturnMaleUnderWeight(){
+        bodyMassIndex.setSex('M');
+        bodyMassIndex.setWeight(20.6);
+        assertEquals("Under weight", bodyMassIndex.getBodyMassIndex());
+    }
+
+    @Test
+    public void shouldReturnMaleNormalWeight(){
+        bodyMassIndex.setSex('M');
+        bodyMassIndex.setWeight(26.3);
+        assertEquals("Normal weight", bodyMassIndex.getBodyMassIndex());
+    }
+
+    @Test
+    public void shouldReturnMaleMarginallyOverweight(){
+        bodyMassIndex.setSex('M');
+        bodyMassIndex.setWeight(27.7);
+        assertEquals("Marginally overweight", bodyMassIndex.getBodyMassIndex());
+    }
+
+    @Test
+    public void shouldReturnMaleOverIdealWeight(){
+        bodyMassIndex.setSex('M');
+        bodyMassIndex.setWeight(31.0);
+        assertEquals("Over ideal weight", bodyMassIndex.getBodyMassIndex());
+    }
+
+    @Test
+    public void shouldReturnMaleObese(){
+        bodyMassIndex.setSex('M');
+        bodyMassIndex.setWeight(31.1);
+        assertEquals("Obese", bodyMassIndex.getBodyMassIndex());
+    }
 }
