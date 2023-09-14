4dc4c4df8d84185f597dec97c91b5c28b6aecd28
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-09-14 13:36:55 -0300

    Create `calculateAliquot` method

--- src/main/java/aliquot/Aliquot.java
+++ src/main/java/aliquot/Aliquot.java
@@ -16,6 +16,32 @@ public class Aliquot {
         this.setProductValue(productValue);
     }
 
+    public double calculateAliquot(){
+        double productValueWithAliquot;
+        if ("AC".equals(this.getFederationUnit()) || "AL".equals(this.getFederationUnit()) ||
+                "ES".equals(this.getFederationUnit()) || "GO".equals(this.getFederationUnit()) ||
+                "MT".equals(this.getFederationUnit()) || "MS".equals(this.getFederationUnit()) ||
+                "PA".equals(this.getFederationUnit()) || "RR".equals(this.getFederationUnit()) ||
+                "SC".equals(this.getFederationUnit())) {
+            productValueWithAliquot = this.productValue + (this.productValue * 0.17);
+        }
+        else if ("AM".equals(this.getFederationUnit()) || "AP".equals(this.getFederationUnit()) ||
+                "BA".equals(this.getFederationUnit()) || "CE".equals(this.getFederationUnit()) ||
+                "DF".equals(this.getFederationUnit()) || "MA".equals(this.getFederationUnit()) ||
+                "MG".equals(this.getFederationUnit()) || "PB".equals(this.getFederationUnit()) ||
+                "PR".equals(this.getFederationUnit()) || "PE".equals(this.getFederationUnit()) ||
+                "PI".equals(this.getFederationUnit()) || "RN".equals(this.getFederationUnit()) ||
+                "RS".equals(this.getFederationUnit()) || "RJ".equals(this.getFederationUnit()) ||
+                "SP".equals(this.getFederationUnit()) || "SE".equals(this.getFederationUnit()) ||
+                "TO".equals(this.getFederationUnit())){
+            productValueWithAliquot = this.productValue + (this.productValue * 0.18);
+        }
+        else {
+            productValueWithAliquot = this.productValue + (this.productValue * 0.175);
+        }
+        return productValueWithAliquot;
+    }
+
     public String getFederationUnit() {
         return this.federationUnit;
     }
